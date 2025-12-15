bb8fa47b86a04354b06126ec00f835def6ce0c44
جميل البدوي <alrajhyjmyl79@gmail.com>
2025-12-15 23:50:33 +0300

    Create sw.js
    تعديل

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'khairy-cafe-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
